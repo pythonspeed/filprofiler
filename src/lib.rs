63823ca75edf05346185ea984e9d92d23d517741
Itamar Turner-Trauring <itamar@itamarst.org>
2019-12-31 21:05:20 -0500

    Add calloc and mmap.

--- src/lib.rs
+++ src/lib.rs
@@ -48,11 +48,31 @@ fn update_memory_usage_while_in_malloc() {
 }
 
 // Override functions via C ABI, for LD_PRELOAD purposes.
-// TODO: add calloc, realloc, posix_memalign. Probably not mmap?
+// TODO: add realloc, posix_memalign.
 redhook::hook! {
     unsafe fn malloc(size: libc::size_t) -> *mut libc::c_void => my_malloc {
         let result = redhook::real!(malloc)(size);
         update_memory_usage_while_in_malloc();
+        result
+    }
+}
+
+redhook::hook! {
+    unsafe fn calloc(nmemb: libc::size_t, size: libc::size_t) -> *mut libc::c_void => my_calloc {
+        let result = redhook::real!(calloc)(nmemb, size);
+        update_memory_usage_while_in_malloc();
+        result
+    }
+}
+
+redhook::hook! {
+    unsafe fn mmap(addr: *mut libc::c_void, length: libc::size_t, prot: libc::c_int, flags: libc::c_int, fd: libc::c_int, offset: libc::off_t) -> *mut libc::c_void => my_mmap {
+        let result = redhook::real!(mmap)(addr, length, prot, flags, fd, offset);
+        println!("MMAP! {}", flags & (libc::MAP_PRIVATE | libc::MAP_ANONYMOUS));
+        if (flags & (libc::MAP_PRIVATE | libc::MAP_ANONYMOUS)) != 0 {
+            // This suggests mmmap() used for allocation:
+            update_memory_usage_while_in_malloc();
+        }
         result
     }
 }
