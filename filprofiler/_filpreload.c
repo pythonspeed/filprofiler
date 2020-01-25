d6f68e68e81ce3117d262e566d5224822472aa24
Itamar Turner-Trauring <itamar@itamarst.org>
2020-01-25 17:07:36 -0500

    Reformat with clang.

--- filprofiler/_filpreload.c
+++ filprofiler/_filpreload.c
@@ -1,14 +1,15 @@
 #define _GNU_SOURCE
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <dlfcn.h>
 #include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
 
 // Underlying APIs we're wrapping:
-static void* (*underlying_real_mmap)(void *addr, size_t length, int prot, int flags, int fd, off_t offset) = 0;
-static void (*underlying_real_free)(void* addr) = 0;
+static void *(*underlying_real_mmap)(void *addr, size_t length, int prot,
+                                     int flags, int fd, off_t offset) = 0;
+static void (*underlying_real_free)(void *addr) = 0;
 
 // The internal API we're notifying of allocations:
 static void (*add_allocation_hook)(size_t address, size_t length) = 0;
@@ -23,13 +24,14 @@ static void __attribute__((constructor)) constructor() {
   if (initialized) {
     return;
   }
-  if (sizeof((void*)0) != sizeof((size_t)0)) {
+  if (sizeof((void *)0) != sizeof((size_t)0)) {
     fprintf(stderr, "BUG: expected size of size_t and void* to be the same.\n");
     exit(1);
   }
-  void* lib = dlopen(getenv("FIL_API_LIBRARY"), RTLD_LAZY | RTLD_DEEPBIND);
+  void *lib = dlopen(getenv("FIL_API_LIBRARY"), RTLD_LAZY | RTLD_DEEPBIND);
   if (!lib) {
-    fprintf(stderr, "Couldn't load libpymemprofile_api.so library: %s\n", dlerror());
+    fprintf(stderr, "Couldn't load libpymemprofile_api.so library: %s\n",
+            dlerror());
     exit(1);
   }
   add_allocation_hook = dlsym(lib, "pymemprofile_add_allocation");
@@ -61,8 +63,8 @@ extern void *__libc_calloc(size_t nmemb, size_t size);
 // TODO switch to new add_allocation and free_allocation APIs
 
 // Override memory-allocation functions:
-__attribute__ ((visibility("default"))) void* malloc(size_t size)  {
-  void* result = __libc_malloc(size);
+__attribute__((visibility("default"))) void *malloc(size_t size) {
+  void *result = __libc_malloc(size);
   if (size > 1000 && result != NULL && !will_i_be_reentrant && initialized) {
     will_i_be_reentrant = 1;
     add_allocation_hook((size_t)result, size);
@@ -71,8 +73,8 @@ __attribute__ ((visibility("default"))) void* malloc(size_t size)  {
   return result;
 }
 
-__attribute__ ((visibility("default"))) void* calloc(size_t nmemb, size_t size) {
-  void* result = __libc_calloc(nmemb, size);
+__attribute__((visibility("default"))) void *calloc(size_t nmemb, size_t size) {
+  void *result = __libc_calloc(nmemb, size);
   size_t allocated = nmemb * size;
   if (allocated > 1000 && !will_i_be_reentrant && initialized) {
     will_i_be_reentrant = 1;
@@ -82,7 +84,7 @@ __attribute__ ((visibility("default"))) void* calloc(size_t nmemb, size_t size) 
   return result;
 }
 
-__attribute__ ((visibility("default"))) void free(void* addr) {
+__attribute__((visibility("default"))) void free(void *addr) {
   if (!initialized) {
     // Well, we're going to leak a little memory, but, such is life...
     return;
@@ -96,15 +98,13 @@ __attribute__ ((visibility("default"))) void free(void* addr) {
 }
 
 /*
-__attribute__ ((visibility("default"))) void* mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset) {
-  if (!initialized) {
-    constructor();
+__attribute__ ((visibility("default"))) void* mmap(void *addr, size_t length,
+int prot, int flags, int fd, off_t offset) { if (!initialized) { constructor();
   }
   void* result = underlying_real_mmap(addr, length, prot, flags, fd, offset);
   fprintf(stdout, "MMAP!\n");
-  if ((flags & (MAP_PRIVATE | MAP_ANONYMOUS)) && !will_i_be_reentrant && initialized) {
-    will_i_be_reentrant = 1;
-    update_memory_usage();
+  if ((flags & (MAP_PRIVATE | MAP_ANONYMOUS)) && !will_i_be_reentrant &&
+initialized) { will_i_be_reentrant = 1; update_memory_usage();
     will_i_be_reentrant = 0;
   }
   return result;
