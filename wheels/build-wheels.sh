bd388eac97436510feb532d23666ad0ff6bc85b9
Itamar Turner-Trauring <itamar@itamarst.org>
2020-03-02 20:42:59 -0500

    Now need per-Python _filpreload, since it uses Python APIs.

--- wheels/build-wheels.sh
+++ wheels/build-wheels.sh
@@ -7,10 +7,19 @@ cd /src
 mkdir -p dist
 PATH=/opt/python/cp38-cp38/bin/:$PATH make clean
 make filprofiler/libpymemprofile_api.so
-make filprofiler/_filpreload.so
+
+PATH=/opt/python/cp36-cp36m/bin/:$PATH make filprofiler/_filpreload.so
 /opt/python/cp36-cp36m/bin/pip wheel . -w /tmp/wheel
+rm -f filprofiler/_filpreload.so
+
+PATH=/opt/python/cp37-cp37m/bin/:$PATH make filprofiler/_filpreload.so
 /opt/python/cp37-cp37m/bin/pip wheel . -w /tmp/wheel
+rm -f filprofiler/_filpreload.so
+
+PATH=/opt/python/cp38-cp38m/bin/:$PATH make filprofiler/_filpreload.so
 /opt/python/cp38-cp38/bin/pip wheel . -w /tmp/wheel
+rm -f filprofiler/_filpreload.so
+
 auditwheel repair -w dist/ /tmp/wheel/filprofiler*36*whl
 auditwheel repair -w dist/ /tmp/wheel/filprofiler*37*whl
 auditwheel repair -w dist/ /tmp/wheel/filprofiler*38*whl
