22fdb687ee635caf3453b85e96bcede104cf2a2e
Itamar Turner-Trauring <itamar@itamarst.org>
2020-06-18 14:16:27 -0400

    A test for library_path().

---
+++ filprofiler/tests/test_utils.py
@@ -0,0 +1,16 @@
+"""Tests for filprofiler._utils."""
+
+import os
+import sys
+
+from .._utils import library_path
+
+
+def test_library_path():
+    """The library is found and has the right suffix."""
+    path = library_path("_filpreload")
+    assert os.path.exists(path)
+    if sys.platform == "darwin":
+        assert path.endswith(".dylib")
+    else:
+        assert path.endswith(".so")
