52d43a7d47a8d0fbc4345ba4c06d261a550d9f19
Itamar Turner-Trauring <itamar@itamarst.org>
2020-08-20 16:53:08 -0400

    JavaScript works.

--- filprofiler/_ipython.py
+++ filprofiler/_ipython.py
@@ -7,7 +7,7 @@ from tempfile import mkdtemp
 from pathlib import Path
 
 from IPython.core.magic import Magics, magics_class, cell_magic
-from IPython.display import SVG, display
+from IPython.display import IFrame, display
 
 from ._tracer import start_tracing, stop_tracing
 
@@ -19,11 +19,11 @@ class FilMagics(Magics):
     @cell_magic
     def filprofile(self, line, cell):
         """Memory profile the code in the cell."""
-        tempdir = mkdtemp()
+        tempdir = "fil-result"
         start_tracing(tempdir)
         try:
             self.shell.run_cell(cell)
         finally:
             index_html_path = stop_tracing(tempdir)
             svg_path = Path(index_html_path).parent / "peak-memory.svg"
-            display(SVG(filename=svg_path))
+            display(IFrame(svg_path, width="100%", height="600"))
