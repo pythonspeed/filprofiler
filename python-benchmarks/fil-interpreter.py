0e1e9b2243b19b2a320a30b88ce2bc0f238bb1f3
Itamar Turner-Trauring <itamar@itamarst.org>
2020-08-26 14:42:40 -0400

    Test for raising exceptions.

--- python-benchmarks/fil-interpreter.py
+++ python-benchmarks/fil-interpreter.py
@@ -49,28 +49,40 @@ def test_temporary_profiling(tmpdir):
     test_no_profiling()
 
 
-def test_ipython_profiling(tmpdir):
-    """Profiling can be run via IPython magic."""
-    cwd = os.getcwd()
-    os.chdir(tmpdir)
+def run_in_ipython_shell(code_cells):
+    """Run a list of strings in IPython.
+
+    Returns parsed allocations.
+    """
     InteractiveShell.clear_instance()
 
     shell = InteractiveShell.instance(display_pub_class=CapturingDisplayPublisher)
-    shell.run_cell("%load_ext filprofiler")
-    shell.run_cell(
-        """\
-%%filprofile
-import numpy as np
-arr = np.ones((1024, 1024, 4), dtype=np.uint64)  # 32MB
-"""
-    )
+    for code in code_cells:
+        shell.run_cell(code)
     InteractiveShell.clear_instance()
-
-    html = shell.display_pub.outputs[0]["data"]["text/html"]
+    html = shell.display_pub.outputs[-1]["data"]["text/html"]
     assert "iframe" in html
     [svg_path] = re.findall('src="([^"]*)"', html)
     assert svg_path.endswith("peak-memory.svg")
     resultdir = Path(svg_path).parent.parent
+
+    return get_allocations(resultdir)
+
+
+def test_ipython_profiling(tmpdir):
+    """Profiling can be run via IPython magic."""
+    cwd = os.getcwd()
+    os.chdir(tmpdir)
+    allocations = run_in_ipython_shell(
+        [
+            "%load_ext filprofiler",
+            """\
+%%filprofile
+import numpy as np
+arr = np.ones((1024, 1024, 4), dtype=np.uint64)  # 32MB
+""",
+        ]
+    )
 
     # Allocations were tracked:
     import numpy.core.numeric
@@ -79,8 +91,42 @@ arr = np.ones((1024, 1024, 4), dtype=np.uint64)  # 32MB
         (re.compile("<ipython-input-1-.*"), "__magic_run_with_fil", 3),
         (numpy.core.numeric.__file__, "ones", ANY),
     )
-    allocations = get_allocations(resultdir)
     assert match(allocations, {path: big}, as_mb) == pytest.approx(32, 0.1)
+
+    # Profiling stopped:
+    test_no_profiling()
+
+
+def test_ipython_exception_while_profiling(tmpdir):
+    """
+    Profiling can be run via IPython magic, still profiles and shuts down
+    correctly on an exception.
+
+    This will log a RuntimeError. That is expected.
+    """
+    cwd = os.getcwd()
+    os.chdir(tmpdir)
+    allocations = run_in_ipython_shell(
+        [
+            "%load_ext filprofiler",
+            """\
+%%filprofile
+import numpy as np
+arr = np.ones((1024, 1024, 2), dtype=np.uint64)  # 16MB
+raise RuntimeError("The test will log this, it's OK.")
+arr = np.ones((1024, 1024, 8), dtype=np.uint64)  # 64MB
+""",
+        ]
+    )
+
+    # Allocations were tracked:
+    import numpy.core.numeric
+
+    path = (
+        (re.compile("<ipython-input-1-.*"), "__magic_run_with_fil", 3),
+        (numpy.core.numeric.__file__, "ones", ANY),
+    )
+    assert match(allocations, {path: big}, as_mb) == pytest.approx(16, 0.1)
 
     # Profiling stopped:
     test_no_profiling()
