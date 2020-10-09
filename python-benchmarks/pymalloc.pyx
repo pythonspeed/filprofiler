737f760b4e82c07529e85dae9a1bf15523f35a52
Itamar Turner-Trauring <itamar@itamarst.org>
2020-10-09 13:56:00 -0400

    Demonstration script for the non-scalability of multithreading.

--- python-benchmarks/pymalloc.pyx
+++ python-benchmarks/pymalloc.pyx
@@ -34,3 +34,14 @@ def pyallocation_api():
 def pyposix_memalign():
     cdef void *result;
     return posix_memalign(&result, 64, 1024 * 1024 * 15)
+
+
+def lots_of_allocs():
+    cdef uint64_t i
+    with nogil:
+        for i in range(10000000):
+            p = <uint64_t*>malloc(16)
+            p[0] = 1
+            free(p)
+    # Garbage, but without this the compiler optimizes the whole loop out.
+    return p[0]
