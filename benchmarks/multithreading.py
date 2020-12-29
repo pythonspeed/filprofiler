df5cc5065edd93d4c3e0bf834696e0aa4c473a36
Itamar Turner-Trauring <itamar@itamarst.org>
2020-12-29 11:55:20 -0500

    Run in main thread if there's only one.

--- benchmarks/multithreading.py
+++ benchmarks/multithreading.py
@@ -7,11 +7,16 @@ from pymalloc import lots_of_allocs
 
 def main():
     start = time.time()
-    threads = [Thread(target=lots_of_allocs) for i in range(int(sys.argv[1]))]
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
+    num_threads = int(sys.argv[1])
+    # If there's only one-thread, just run in main thread:
+    if num_threads == 1:
+        lots_of_allocs()
+    else:
+        threads = [Thread(target=lots_of_allocs) for i in range(num_threads)]
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
     print("Elapsed:", time.time() - start)
 
 
