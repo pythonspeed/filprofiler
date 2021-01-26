c65a3bce40d8c73e6dae8de8cded8760884d3e95
Itamar Turner-Trauring <itamar@itamarst.org>
2021-01-26 13:49:31 -0500

    Try to make malloc()/free() loop less noisy in the face of minor changes.

--- benchmarks/pymalloc.pyx
+++ benchmarks/pymalloc.pyx
@@ -2,11 +2,16 @@ from libc.stdlib cimport malloc, free
 from libc.stdint cimport uint64_t
 
 def lots_of_allocs():
-    cdef uint64_t i
+    cdef uint64_t i, j
+    cdef uint64_t *ps[1000]
     with nogil:
-        for i in range(10000000):
-            p = <uint64_t*>malloc(16)
-            p[0] = 1
-            free(p)
-    # Garbage, but without this the compiler optimizes the whole loop out.
-    return p[0]
+        for j in range(100):
+            for i in range(1000):
+                ps[i] = <uint64_t*>malloc(16)
+                ps[i][0] = 1
+            # If we just free everything, we get... noise based on previous
+            # allocations. So try to smooth with increasing allocations.
+            for i in range(500):
+                free(ps[i])
+    # Garbage, but without this the compiler might optimize the whole loop out.
+    return <uint64_t>ps[0]
