8d180eb2aaf9dd3040f905b727799cb9d37100b6
Itamar Turner-Trauring <itamar@itamarst.org>
2021-10-12 13:09:28 -0400

    Get rid of CFLAGS magic, use build.rs instead.

--- filpreload/build.rs
+++ filpreload/build.rs
@@ -1,3 +1,19 @@
+use std::process::Command;
+
+/// Get paths for C compilation builds, e.g. "include" or "platinclude".
+/// TODO this is copy/pasted multiple times...
+fn get_python_path(pathname: &str) -> String {
+    let output = Command::new("python")
+        .arg("-c")
+        .arg(format!(
+            "import sysconfig; print(sysconfig.get_path('{}'))",
+            pathname
+        ))
+        .output()
+        .unwrap();
+    String::from_utf8(output.stdout).unwrap().trim().into()
+}
+
 fn main() -> Result<(), std::io::Error> {
     println!("cargo:rerun-if-changed=src/_filpreload.c");
     let cur_dir = std::env::current_dir()?;
@@ -24,10 +40,14 @@ fn main() -> Result<(), std::io::Error> {
         println!("cargo:rustc-cdylib-link-arg=-Wl,--defsym=mmap64=fil_mmap_impl");
     };
 
-    // Compilation options are taken from CFLAGS environment variable set in
-    // setup.py based on Python's build configuration.
+    // Compilation options are taken from Python's build configuration.
     cc::Build::new()
         .file("src/_filpreload.c")
+        .include(get_python_path("include"))
+        .include(get_python_path("platinclude"))
+        .define("_GNU_SOURCE", "1")
+        .define("NDEBUG", "1")
+        .flag("-fno-omit-frame-pointer")
         .compile("_filpreload");
     Ok(())
 }
