9688b84ee1d22bd28c4867300100e3f11a441c2b
Itamar Turner-Trauring <itamar@itamarst.org>
2022-03-27 18:48:04 -0400

    Need unsafe wrapper here too.

--- filpreload/src/lib.rs
+++ filpreload/src/lib.rs
@@ -342,7 +342,7 @@ impl pymemprofile_api::mmap::MmapAPI for FilMmapAPI {
 #[cfg(target_os = "macos")]
 #[no_mangle]
 pub extern "C" fn reimplemented_munmap(addr: *mut c_void, len: usize) -> c_int {
-    return pymemprofile_api::mmap::munmap_wrapper(addr, len, &FilMmapAPI {});
+    return unsafe { pymemprofile_api::mmap::munmap_wrapper(addr, len, &FilMmapAPI {}) };
 }
 
 /// On Linux we're using same name as the API we're replacing.
