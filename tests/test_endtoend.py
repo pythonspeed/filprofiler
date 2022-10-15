f08f0de497361c0395df7b92a7b066177a941836
Itamar Turner-Trauring <itamar@pythonspeed.com>
2022-10-15 18:40:41 -0400

    Stricter tests.

--- tests/test_endtoend.py
+++ tests/test_endtoend.py
@@ -326,7 +326,7 @@ def test_out_of_memory_detection_disabled():
         assert False, "process succeeded?!"
 
 
-def get_systemd_run_args(available_memory):
+def get_systemd_run_args(memory_limit):
     """
     Figure out if we're on system with cgroups v2, or not, and return
     appropriate systemd-run args.
@@ -340,7 +340,7 @@ def get_systemd_run_args(available_memory):
         "--gid",
         str(os.getegid()),
         "-p",
-        f"MemoryLimit={available_memory // 4}B",
+        f"MemoryLimit={memory_limit}B",
         "--scope",
         "--same-dir",
     ]
@@ -364,10 +364,11 @@ def test_out_of_memory_slow_leak_cgroups():
     """
     available_memory = psutil.virtual_memory().available
     script = TEST_SCRIPTS / "oom-slow.py"
+    memory_limit = available_memory // 4
     output_dir = profile(
         script,
         expect_exit_code=53,
-        argv_prefix=get_systemd_run_args(available_memory),
+        argv_prefix=get_systemd_run_args(memory_limit),
     )
     time.sleep(10)  # wait for child process to finish
     allocations = get_allocations(
@@ -385,7 +386,9 @@ def test_out_of_memory_slow_leak_cgroups():
     # Should've allocated at least a little before running out, unless testing
     # environment is _really_ restricted, in which case other tests would've
     # failed.
-    assert match(allocations, {expected_alloc: big}, as_mb) > 100
+    failed_alloc_size = match(allocations, {expected_alloc: big}, lambda kb: kb * 1024)
+    assert failed_alloc_size > 0.7 * memory_limit
+    assert failed_alloc_size < 1.3 * memory_limit
 
 
 def test_external_behavior():
@@ -506,7 +509,7 @@ def test_jupyter(tmpdir):
             continue
         else:
             actual_path = key
-    assert actual_path != None
+    assert actual_path is not None
     assert actual_path[0][0] != actual_path[1][0]  # code is in different cells
     path2 = (
         (re.compile(".*ipy.*"), "__magic_run_with_fil", 2),
